83101b8c9adca70182fe644cdf469d4500ee32e0
Ross Fisher <zorrobyte@gmail.com>
2019-08-02 12:26:01 -0400

    Delta up

--- panda/board/safety/safety_toyota.h
+++ panda/board/safety/safety_toyota.h
@@ -3,7 +3,7 @@ const int TOYOTA_MAX_TORQUE = 1500;       // max torque cmd allowed ever
 
 // rate based torque limit + stay within actually applied
 // packet is sent at 100hz, so this limit is 1000/sec
-const int TOYOTA_MAX_RATE_UP = 10;        // ramp up slow
+const int TOYOTA_MAX_RATE_UP = 15;        // ramp up slow
 const int TOYOTA_MAX_RATE_DOWN = 25;      // ramp down fast
 const int TOYOTA_MAX_TORQUE_ERROR = 350;  // max torque cmd in excess of torque motor
 
