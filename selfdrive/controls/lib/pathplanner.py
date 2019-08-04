52d56f20d6b8f710124d1a7f0908c55d521c9727
Greg Esmond <gernby2002@yahoo.com>
2019-08-04 10:05:42 -0500

    fixed pathplanner, added accel limit

--- selfdrive/controls/lib/pathplanner.py
+++ selfdrive/controls/lib/pathplanner.py
@@ -11,6 +11,7 @@ from selfdrive.controls.lib.model_parser import ModelParser
 import selfdrive.messaging as messaging
 
 LOG_MPC = os.environ.get('LOG_MPC', False)
+MAX_STEER_ACCEL = 10.5
 
 def calc_states_after_delay(states, v_ego, steer_angle, curvature_factor, steer_ratio, delay):
   states[0].x = v_ego * delay
@@ -61,7 +62,6 @@ class PathPlanner(object):
     angle_steers = sm['controlsState'].dampAngleSteers
     cur_time = sec_since_boot()
     angle_offset_average = sm['liveParameters'].angleOffsetAverage
-    angle_offset_bias = sm['controlsState'].lateralControlState.pidState.angleBias + angle_offset_average
 
     self.MP.update(v_ego, sm['model'])
 
@@ -74,7 +74,7 @@ class PathPlanner(object):
     self.p_poly = list(self.MP.p_poly)
 
     # account for actuation delay
-    self.cur_state = calc_states_after_delay(self.cur_state, v_ego, angle_steers - angle_offset_bias, curvature_factor, VM.sR, CP.steerActuatorDelay)
+    self.cur_state = calc_states_after_delay(self.cur_state, v_ego, angle_steers - angle_offset_average, curvature_factor, VM.sR, CP.steerActuatorDelay)
     self.angle_steers_des_prev = np.interp(cur_time, self.mpc_times, self.mpc_angles)
 
     v_ego_mpc = max(v_ego, 5.0)  # avoid mpc roughness due to low speed
@@ -90,7 +90,7 @@ class PathPlanner(object):
       self.mpc_times[0] = sm.logMonoTime['model'] * 1e-9
       for i in range(1,6):
         self.mpc_times[i] = self.mpc_times[i-1] + 0.05
-        self.mpc_rates[i-1] = float(math.degrees(self.mpc_solution[0].rate[i-1] * VM.sR))
+        self.mpc_rates[i-1] = np.clip(float(math.degrees(self.mpc_solution[0].rate[i-1] * VM.sR)), self.mpc_rates[i-1] - MAX_STEER_ACCEL, self.mpc_rates[i-1] + MAX_STEER_ACCEL)
         self.mpc_angles[i] = (0.05 * self.mpc_rates[i-1] + self.mpc_angles[i-1])
       self.mpc_angles[6] = (0.15 * self.mpc_rates[5] + self.mpc_angles[5])
       self.mpc_times[6] = self.mpc_times[5] + 0.15
