1871b2326efce5dd5784ecc550df237a9f46158a
Ross Fisher <zorrobyte@gmail.com>
2019-09-05 19:42:42 -0400

    Update interface.py

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -68,7 +68,7 @@ class CarInterface(object):
       ret.steerRatio = 13.4   # unknown end-to-end spec
       tire_stiffness_factor = 1.   # hand-tune
       ret.mass = 3045. * CV.LB_TO_KG + STD_CARGO_KG
-      ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.2], [0.05]]
+      ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.6], [0.05]]
       ret.lateralTuning.pid.kf = 0.00003   # full torque for 20 deg at 80mph means 0.00007818594
 
       ret.steerActuatorDelay = 1.0 #disabled
