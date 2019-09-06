1ba9bbbcf89f1142a71348ba3ebc0a8b51d5deb8
Ross Fisher <zorrobyte@gmail.com>
2019-09-05 20:43:06 -0400

    generator.cpp
    not sure if controlHorizon actually compiled/did anything

--- selfdrive/controls/lib/lateral_mpc/generator.cpp
+++ selfdrive/controls/lib/lateral_mpc/generator.cpp
@@ -109,9 +109,9 @@ int main( )
   ocp.minimizeLSQEndTerm(QN, hN);
 
   // car can't go backward to avoid "circles"
-  ocp.subjectTo( deg2rad(-90) <= psi <= deg2rad(90));
+  ocp.subjectTo( deg2rad(-30) <= psi <= deg2rad(30));
   // more than absolute max steer angle
-  ocp.subjectTo( deg2rad(-50) <= delta <= deg2rad(50));
+  ocp.subjectTo( deg2rad(-20) <= delta <= deg2rad(20));
   ocp.setNOD(17);
 
   OCPexport mpc(ocp);
