7f8161fda784fb8d62fc8ec017fa56812f69f9a6
Ross Fisher <zorrobyte@gmail.com>
2019-09-05 22:32:14 -0400

    Update interface.py

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -182,7 +182,7 @@ class CarInterface(object):
       tire_stiffness_factor = 0.444
       ret.mass = 4590. * CV.LB_TO_KG + STD_CARGO_KG
       ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.3], [0.05]]
-      ret.lateralTuning.pid.kf = 0.00007818594
+      ret.lateralTuning.pid.kf = 0.00006
 
     ret.centerToFront = ret.wheelbase * 0.44
 
