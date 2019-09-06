cbc9bd64e0a883c927a88795e553c7d6b89f65ee
Ross Fisher <zorrobyte@gmail.com>
2019-09-05 21:33:39 -0400

    Revert "UNSAFE OMG UNSAFE torque params!!1"
    This reverts commit 2f5e297c1c5c483e3de994404499324b5b8a7669.

--- panda/board/safety/safety_toyota.h
+++ panda/board/safety/safety_toyota.h
@@ -3,13 +3,13 @@ const int TOYOTA_MAX_TORQUE = 1500;       // max torque cmd allowed ever
 
 // rate based torque limit + stay within actually applied
 // packet is sent at 100hz, so this limit is 1000/sec
-const int TOYOTA_MAX_RATE_UP = 50;        // ramp up slow
-const int TOYOTA_MAX_RATE_DOWN = 50;      // ramp down fast
-const int TOYOTA_MAX_TORQUE_ERROR = 1000;  // max torque cmd in excess of torque motor
+const int TOYOTA_MAX_RATE_UP = 15;        // ramp up slow
+const int TOYOTA_MAX_RATE_DOWN = 15;      // ramp down fast
+const int TOYOTA_MAX_TORQUE_ERROR = 550;  // max torque cmd in excess of torque motor
 
 // real time torque limit to prevent controls spamming
 // the real time limit is 1500/sec
-const int TOYOTA_MAX_RT_DELTA = 1075;      // max delta torque allowed for real time checks
+const int TOYOTA_MAX_RT_DELTA = 575;      // max delta torque allowed for real time checks
 const uint32_t TOYOTA_RT_INTERVAL = 250000;    // 250ms between real time checks
 
 // longitudinal limits
