795f5558a0a5f0b356ef97301a927765fef4a970
Ross Fisher <zorrobyte@gmail.com>
2019-09-17 12:01:32 -0400

    Revert "Update install_capnp.sh"
    This reverts commit 126046fb169abd07629087e50a4620b1c3bcf0dd.

--- cereal/install_capnp.sh
+++ cereal/install_capnp.sh
@@ -11,17 +11,17 @@ CXXFLAGS="-fPIC" ./configure
 make -j4
 
 # manually build binaries statically
-g++ -std=gnu++11 -I./src -I./src -DKJ_HEADER_WARNINGS -DCAPNP_HEADER_WARNINGS -DCAPNP_INCLUDE_DIR=\"/usr/include\" -pthread -O2 -DNDEBUG -pthread -pthread -o .libs/capnp src/capnp/compiler/module-loader.o src/capnp/compiler/capnp.o  ./.libs/libcapnpc.a ./.libs/libcapnp.a ./.libs/libkj.a -lpthread -pthread
+g++ -std=gnu++11 -I./src -I./src -DKJ_HEADER_WARNINGS -DCAPNP_HEADER_WARNINGS -DCAPNP_INCLUDE_DIR=\"/usr/local/include\" -pthread -O2 -DNDEBUG -pthread -pthread -o .libs/capnp src/capnp/compiler/module-loader.o src/capnp/compiler/capnp.o  ./.libs/libcapnpc.a ./.libs/libcapnp.a ./.libs/libkj.a -lpthread -pthread
 
-g++ -std=gnu++11 -I./src -I./src -DKJ_HEADER_WARNINGS -DCAPNP_HEADER_WARNINGS -DCAPNP_INCLUDE_DIR=\"/usr/include\" -pthread -O2 -DNDEBUG -pthread -pthread -o .libs/capnpc-c++ src/capnp/compiler/capnpc-c++.o  ./.libs/libcapnp.a ./.libs/libkj.a -lpthread -pthread
+g++ -std=gnu++11 -I./src -I./src -DKJ_HEADER_WARNINGS -DCAPNP_HEADER_WARNINGS -DCAPNP_INCLUDE_DIR=\"/usr/local/include\" -pthread -O2 -DNDEBUG -pthread -pthread -o .libs/capnpc-c++ src/capnp/compiler/capnpc-c++.o  ./.libs/libcapnp.a ./.libs/libkj.a -lpthread -pthread
 
-g++ -std=gnu++11 -I./src -I./src -DKJ_HEADER_WARNINGS -DCAPNP_HEADER_WARNINGS -DCAPNP_INCLUDE_DIR=\"/usr/include\" -pthread -O2 -DNDEBUG -pthread -pthread -o .libs/capnpc-capnp src/capnp/compiler/capnpc-capnp.o  ./.libs/libcapnp.a ./.libs/libkj.a -lpthread -pthread
+g++ -std=gnu++11 -I./src -I./src -DKJ_HEADER_WARNINGS -DCAPNP_HEADER_WARNINGS -DCAPNP_INCLUDE_DIR=\"/usr/local/include\" -pthread -O2 -DNDEBUG -pthread -pthread -o .libs/capnpc-capnp src/capnp/compiler/capnpc-capnp.o  ./.libs/libcapnp.a ./.libs/libkj.a -lpthread -pthread
 
-cp .libs/capnp /usr/bin/
-ln -s /usr/bin/capnp /usr/bin/capnpc
-cp .libs/capnpc-c++ /usr/bin/
-cp .libs/capnpc-capnp /usr/bin/
-cp .libs/*.a /usr/lib
+cp .libs/capnp /usr/local/bin/
+ln -s /usr/local/bin/capnp /usr/local/bin/capnpc
+cp .libs/capnpc-c++ /usr/local/bin/
+cp .libs/capnpc-capnp /usr/local/bin/
+cp .libs/*.a /usr/local/lib
 
 cd /tmp
 echo "Installing c-capnp"
@@ -35,5 +35,5 @@ make -j4
 # manually build binaries statically
 gcc -fPIC -o .libs/capnpc-c compiler/capnpc-c.o compiler/schema.capnp.o compiler/str.o  ./.libs/libcapnp_c.a
 
-cp .libs/capnpc-c /usr/bin/
-cp .libs/*.a /usr/lib
+cp .libs/capnpc-c /usr/local/bin/
+cp .libs/*.a /usr/local/lib
