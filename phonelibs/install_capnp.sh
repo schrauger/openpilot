c9e7b92c16c7871199332294bfbaa96f3ac6f778
Ross Fisher <zorrobyte@gmail.com>
2019-09-18 23:50:17 -0400

    Update install_capnp.sh

--- phonelibs/install_capnp.sh
+++ phonelibs/install_capnp.sh
@@ -6,24 +6,24 @@ VERSION=0.6.1
 wget https://capnproto.org/capnproto-c++-${VERSION}.tar.gz
 tar xvf capnproto-c++-${VERSION}.tar.gz
 cd capnproto-c++-${VERSION}
-CXXFLAGS="-fPIC" ./configure
+CXXFLAGS="-fPIC" ./configure --prefix=/data/data/com.termux/files/usr
 
 make -j4
 make install
 
 # manually build binaries statically
-g++ -std=gnu++11 -I./src -I./src -DKJ_HEADER_WARNINGS -DCAPNP_HEADER_WARNINGS -DCAPNP_INCLUDE_DIR=\"/usr/local/include\" -pthread -O2 -DNDEBUG -pthread -pthread -o .libs/capnp src/capnp/compiler/module-loader.o src/capnp/compiler/capnp.o  ./.libs/libcapnpc.a ./.libs/libcapnp.a ./.libs/libkj.a -lpthread -pthread
+g++ -std=gnu++11 -I./src -I./src -DKJ_HEADER_WARNINGS -DCAPNP_HEADER_WARNINGS -DCAPNP_INCLUDE_DIR=\"/data/data/com.termux/files/usr/local/include\" -pthread -O2 -DNDEBUG -pthread -pthread -o .libs/capnp src/capnp/compiler/module-loader.o src/capnp/compiler/capnp.o  ./.libs/libcapnpc.a ./.libs/libcapnp.a ./.libs/libkj.a -lpthread -pthread
 
-g++ -std=gnu++11 -I./src -I./src -DKJ_HEADER_WARNINGS -DCAPNP_HEADER_WARNINGS -DCAPNP_INCLUDE_DIR=\"/usr/local/include\" -pthread -O2 -DNDEBUG -pthread -pthread -o .libs/capnpc-c++ src/capnp/compiler/capnpc-c++.o  ./.libs/libcapnp.a ./.libs/libkj.a -lpthread -pthread
+g++ -std=gnu++11 -I./src -I./src -DKJ_HEADER_WARNINGS -DCAPNP_HEADER_WARNINGS -DCAPNP_INCLUDE_DIR=\"/data/data/com.termux/files/usr/local/include\" -pthread -O2 -DNDEBUG -pthread -pthread -o .libs/capnpc-c++ src/capnp/compiler/capnpc-c++.o  ./.libs/libcapnp.a ./.libs/libkj.a -lpthread -pthread
 
-g++ -std=gnu++11 -I./src -I./src -DKJ_HEADER_WARNINGS -DCAPNP_HEADER_WARNINGS -DCAPNP_INCLUDE_DIR=\"/usr/local/include\" -pthread -O2 -DNDEBUG -pthread -pthread -o .libs/capnpc-capnp src/capnp/compiler/capnpc-capnp.o  ./.libs/libcapnp.a ./.libs/libkj.a -lpthread -pthread
+g++ -std=gnu++11 -I./src -I./src -DKJ_HEADER_WARNINGS -DCAPNP_HEADER_WARNINGS -DCAPNP_INCLUDE_DIR=\"/data/data/com.termux/files/usr/local/include\" -pthread -O2 -DNDEBUG -pthread -pthread -o .libs/capnpc-capnp src/capnp/compiler/capnpc-capnp.o  ./.libs/libcapnp.a ./.libs/libkj.a -lpthread -pthread
 
-cp .libs/capnp /usr/local/bin/
-rm /usr/local/bin/capnpc
-ln -s /usr/local/bin/capnp /usr/local/bin/capnpc
-cp .libs/capnpc-c++ /usr/local/bin/
-cp .libs/capnpc-capnp /usr/local/bin/
-cp .libs/*.a /usr/local/lib
+cp .libs/capnp /data/data/com.termux/files/usr/local/bin/
+rm /data/data/com.termux/files/usr/local/bin/capnpc
+ln -s /data/data/com.termux/files/usr/local/bin/capnp /data/data/com.termux/files/usr/local/bin/capnpc
+cp .libs/capnpc-c++ /data/data/com.termux/files/usr/local/bin/
+cp .libs/capnpc-capnp /data/data/com.termux/files/usr/local/bin/
+cp .libs/*.a /data/data/com.termux/files/usr/local/lib
 
 cd /tmp
 echo "Installing c-capnp"
@@ -38,5 +38,5 @@ make install
 # manually build binaries statically
 gcc -fPIC -o .libs/capnpc-c compiler/capnpc-c.o compiler/schema.capnp.o compiler/str.o  ./.libs/libcapnp_c.a
 
-cp .libs/capnpc-c /usr/local/bin/
-cp .libs/*.a /usr/local/lib
+cp .libs/capnpc-c /data/data/com.termux/files/usr/local/bin/
+cp .libs/*.a /data/data/com.termux/files/usr/local/lib
