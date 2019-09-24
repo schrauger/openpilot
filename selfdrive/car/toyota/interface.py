b166cdf77a4616bf4a83e5d86467a3fe2747abe9
Ross Fisher <zorrobyte@gmail.com>
2019-09-24 16:21:41 -0400

    disable SAD and lower timeconstant

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -73,7 +73,7 @@ class CarInterface(object):
       ret.lateralTuning.init('indi')
       ret.lateralTuning.indi.innerLoopGain = 4.0
       ret.lateralTuning.indi.outerLoopGain = 3.0
-      ret.lateralTuning.indi.timeConstant = 1.0
+      ret.lateralTuning.indi.timeConstant = 0.5
       ret.lateralTuning.indi.actuatorEffectiveness = 1.0
 
       # TODO: Determine if this is better than INDI
@@ -88,7 +88,7 @@ class CarInterface(object):
       # ret.lateralTuning.lqr.l = [0.03233671, 0.03185757]
       # ret.lateralTuning.lqr.dcGain = 0.002237852961363602
 
-      ret.steerActuatorDelay = 0.5
+      ret.steerActuatorDelay = 1.0
 
     elif candidate in [CAR.RAV4, CAR.RAV4H]:
       stop_and_go = True if (candidate in CAR.RAV4H) else False
