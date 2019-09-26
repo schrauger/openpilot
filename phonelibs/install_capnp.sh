db5991e7de5456c1bd232506ddd53139794991ad
Ross Fisher <zorrobyte@gmail.com>
2019-09-26 09:23:08 -0400

    Update install_capnp.sh

--- phonelibs/install_capnp.sh
+++ phonelibs/install_capnp.sh
@@ -2,28 +2,12 @@ set -e
 echo "Installing capnp"
 
 cd /tmp
-VERSION=0.6.1
-wget https://capnproto.org/capnproto-c++-${VERSION}.tar.gz
-tar xvf capnproto-c++-${VERSION}.tar.gz
-cd capnproto-c++-${VERSION}
-CXXFLAGS="-fPIC" ./configure
-
-make -j4
-make install
-
-# manually build binaries statically
-g++ -std=gnu++11 -I./src -I./src -DKJ_HEADER_WARNINGS -DCAPNP_HEADER_WARNINGS -DCAPNP_INCLUDE_DIR=\"/usr/local/include\" -pthread -O2 -DNDEBUG -pthread -pthread -o .libs/capnp src/capnp/compiler/module-loader.o src/capnp/compiler/capnp.o  ./.libs/libcapnpc.a ./.libs/libcapnp.a ./.libs/libkj.a -lpthread -pthread
-
-g++ -std=gnu++11 -I./src -I./src -DKJ_HEADER_WARNINGS -DCAPNP_HEADER_WARNINGS -DCAPNP_INCLUDE_DIR=\"/usr/local/include\" -pthread -O2 -DNDEBUG -pthread -pthread -o .libs/capnpc-c++ src/capnp/compiler/capnpc-c++.o  ./.libs/libcapnp.a ./.libs/libkj.a -lpthread -pthread
-
-g++ -std=gnu++11 -I./src -I./src -DKJ_HEADER_WARNINGS -DCAPNP_HEADER_WARNINGS -DCAPNP_INCLUDE_DIR=\"/usr/local/include\" -pthread -O2 -DNDEBUG -pthread -pthread -o .libs/capnpc-capnp src/capnp/compiler/capnpc-capnp.o  ./.libs/libcapnp.a ./.libs/libkj.a -lpthread -pthread
-
-cp .libs/capnp /usr/local/bin/
-rm /usr/local/bin/capnpc
-ln -s /usr/local/bin/capnp /usr/local/bin/capnpc
-cp .libs/capnpc-c++ /usr/local/bin/
-cp .libs/capnpc-capnp /usr/local/bin/
-cp .libs/*.a /usr/local/lib
+curl -O https://capnproto.org/capnproto-c++-0.7.0.tar.gz
+tar zxf capnproto-c++-0.7.0.tar.gz
+cd capnproto-c++-0.7.0
+./configure
+make -j6 check
+sudo make install
 
 cd /tmp
 echo "Installing c-capnp"
@@ -42,9 +26,3 @@ autoreconf -f -i -s
 ./configure
 make -j4
 make install
-
-# manually build binaries statically
-gcc -fPIC -o .libs/capnpc-c compiler/capnpc-c.o compiler/schema.capnp.o compiler/str.o  ./.libs/libcapnp_c.a
-
-cp .libs/capnpc-c /usr/local/bin/
-cp .libs/*.a /usr/local/lib
