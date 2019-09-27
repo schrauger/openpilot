20e7889cacb8fffc965f5bdcea07455cbe49b542
Ross Fisher <zorrobyte@gmail.com>
2019-09-27 13:54:22 -0400

    Update install_capnp.sh

--- phonelibs/install_capnp.sh
+++ phonelibs/install_capnp.sh
@@ -28,7 +28,7 @@ cp .libs/*.a /data/data/com.termux/files/usr/lib
 
 cd
 echo "Installing c-capnp"
-git clone https://github.com/zorrobyte/c-capnproto.git
+git clone https://github.com/commaai/c-capnproto.git
 cd c-capnproto
 git submodule update --init --recursive
 autoreconf -f -i -s
