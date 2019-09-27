7bb200382523d815dd67392312b5f521e205836e
Ross Fisher <zorrobyte@gmail.com>
2019-09-27 11:50:38 -0400

    Revert "Update install_capnp.sh"
    This reverts commit 9105fc825f731eb1f23ed61a8d711abf0f3ffe7e.

--- cereal/install_capnp.sh
+++ cereal/install_capnp.sh
@@ -12,7 +12,17 @@ make -j4
 make install
 
 # manually build binaries statically
+g++ -std=gnu++11 -I./src -I./src -DKJ_HEADER_WARNINGS -DCAPNP_HEADER_WARNINGS -DCAPNP_INCLUDE_DIR=\"/data/data/com.termux/files/usr/local/include\" -pthread -O2 -DNDEBUG -pthread -pthread -o .libs/capnp src/capnp/compiler/module-loader.o src/capnp/compiler/capnp.o  ./.libs/libcapnpc.a ./.libs/libcapnp.a ./.libs/libkj.a -lpthread -pthread
 
+g++ -std=gnu++11 -I./src -I./src -DKJ_HEADER_WARNINGS -DCAPNP_HEADER_WARNINGS -DCAPNP_INCLUDE_DIR=\"/data/data/com.termux/files/usr/local/include\" -pthread -O2 -DNDEBUG -pthread -pthread -o .libs/capnpc-c++ src/capnp/compiler/capnpc-c++.o  ./.libs/libcapnp.a ./.libs/libkj.a -lpthread -pthread
+
+g++ -std=gnu++11 -I./src -I./src -DKJ_HEADER_WARNINGS -DCAPNP_HEADER_WARNINGS -DCAPNP_INCLUDE_DIR=\"/data/data/com.termux/files/usr/local/include\" -pthread -O2 -DNDEBUG -pthread -pthread -o .libs/capnpc-capnp src/capnp/compiler/capnpc-capnp.o  ./.libs/libcapnp.a ./.libs/libkj.a -lpthread -pthread
+
+cp .libs/capnp /data/data/com.termux/files/usr/local/bin/
+ln -s /data/data/com.termux/files/usr/local/bin/capnp /data/data/com.termux/files/usr/local/bin/capnpc
+cp .libs/capnpc-c++ /data/data/com.termux/files/usr/local/bin/
+cp .libs/capnpc-capnp /data/data/com.termux/files/usr/local/bin/
+cp .libs/*.a /data/data/com.termux/files/usr/local/lib
 
 cd /tmp
 echo "Installing c-capnp"
@@ -23,3 +33,9 @@ autoreconf -f -i -s
 CXXFLAGS="-fPIC" ./configure
 make -j4
 make install
+
+# manually build binaries statically
+gcc -fPIC -o .libs/capnpc-c compiler/capnpc-c.o compiler/schema.capnp.o compiler/str.o  ./.libs/libcapnp_c.a
+
+cp .libs/capnpc-c /data/data/com.termux/files/usr/local/bin/
+cp .libs/*.a /data/data/com.termux/files/usr/local/lib
