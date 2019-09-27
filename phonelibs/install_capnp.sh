21e68f5eba71125e5e4eebd669857a3123f9faeb
Ross Fisher <zorrobyte@gmail.com>
2019-09-27 11:54:57 -0400

    Update install_capnp.sh

--- phonelibs/install_capnp.sh
+++ phonelibs/install_capnp.sh
@@ -10,18 +10,9 @@ CXXFLAGS="-fPIC" ./configure
 make -j4
 make install
 
-# manually build binaries statically
-g++ -std=gnu++11 -I./src -I./src -DKJ_HEADER_WARNINGS -DCAPNP_HEADER_WARNINGS -DCAPNP_INCLUDE_DIR=\"/data/data/com.termux/files/usr/include\" -pthread -O2 -DNDEBUG -pthread -pthread -o .libs/capnp src/capnp/compiler/module-loader.o src/capnp/compiler/capnp.o  ./.libs/libcapnpc.a ./.libs/libcapnp.a ./.libs/libkj.a -lpthread -pthread
 
-g++ -std=gnu++11 -I./src -I./src -DKJ_HEADER_WARNINGS -DCAPNP_HEADER_WARNINGS -DCAPNP_INCLUDE_DIR=\"/data/data/com.termux/files/usr/include\" -pthread -O2 -DNDEBUG -pthread -pthread -o .libs/capnpc-c++ src/capnp/compiler/capnpc-c++.o  ./.libs/libcapnp.a ./.libs/libkj.a -lpthread -pthread
-
-g++ -std=gnu++11 -I./src -I./src -DKJ_HEADER_WARNINGS -DCAPNP_HEADER_WARNINGS -DCAPNP_INCLUDE_DIR=\"/data/data/com.termux/files/usr/include\" -pthread -O2 -DNDEBUG -pthread -pthread -o .libs/capnpc-capnp src/capnp/compiler/capnpc-capnp.o  ./.libs/libcapnp.a ./.libs/libkj.a -lpthread -pthread
-
-cp .libs/capnp /data/data/com.termux/files/usr/bin/
 ln -s /data/data/com.termux/files/usr/bin/capnp /data/data/com.termux/files/usr/bin/capnpc
-cp .libs/capnpc-c++ /data/data/com.termux/files/usr/bin/
-cp .libs/capnpc-capnp /data/data/com.termux/files/usr/bin/
-cp .libs/*.a /data/data/com.termux/files/usr/lib
+
 
 cd
 echo "Installing c-capnp"
@@ -32,9 +23,3 @@ autoreconf -f -i -s
 CXXFLAGS="-fPIC" ./configure
 make -j4
 make install
-
-# manually build binaries statically
-gcc -fPIC -o .libs/capnpc-c compiler/capnpc-c.o compiler/schema.capnp.o compiler/str.o  ./.libs/libcapnp_c.a
-
-cp .libs/capnpc-c /data/data/com.termux/files/usr/bin/
-cp .libs/*.a /data/data/com.termux/files/usr/lib
